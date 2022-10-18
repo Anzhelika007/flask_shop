ec17a5c41735c8b3ac776781481228a1ffa9ac34
Anzhalika Turlak <turlak.by@gmail.com>
2022-10-18 22:56:19 +0300

    template optimization

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import os
+
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -19,15 +20,20 @@ def create_app():
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(300), nullable=False)
-    price = db.Column()
-    isActive = db.Column()
-    category = db.Column()
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, nullable=True)
+
+    def __repr__(self):
+        return self.title
+
+
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+
+    return render_template('index.html', data=items)
 
 
 @app.route('/about')
@@ -35,9 +41,22 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/catalog')
-def catalog():
-    return render_template('catalog.html')
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Произошла ошибка'
+    else:
+        return render_template('create.html')
 
 
 if __name__ == '__main__':
